023bd34fef0d630208ae719af34a0103aef7316e
Malik Godwin Onimisi <despeauxz@gmail.com>
2019-05-27 10:03:42 +0100

    fixed heroku migration error

--- server/config/database_config.js
+++ server/config/database_config.js
@@ -16,7 +16,7 @@ module.exports = {
         dialect: 'postgres'
     },
     production: {
-        use_env_variables: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL,
         dialect: 'postgres'
     }
 };
